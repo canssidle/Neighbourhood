88ebc6552ff7748122d27c580738978ba53a9074
canssidle <canssidlewairimu@gmail.com>
2019-07-08 11:25:49 +0300

    create the view for viewing a new blog

--- neigborhood/views.py
+++ neigborhood/views.py
@@ -97,33 +97,33 @@ def my_profile(request):
     return render(request,'user_profile.html',{"profile":profile})
 
 
-# @login_required(login_url='/accounts/login/')
-# def user_profile(request,username):
-#     user = User.objects.get(username=username)
-#     profile =Profile.objects.get(username=user)
+@login_required(login_url='/accounts/login/')
+def user_profile(request,username):
+    user = User.objects.get(username=username)
+    profile =Profile.objects.get(username=user)
 
-#     return render(request,'user_profile.html',{"profile":profile})
+    return render(request,'user_profile.html',{"profile":profile})
 
-# @login_required(login_url='/accounts/login/')
-# def new_blogpost(request):
-#     current_user=request.user
-#     profile =Profile.objects.get(username=current_user)
+@login_required(login_url='/accounts/login/')
+def new_blogpost(request):
+    current_user=request.user
+    profile =Profile.objects.get(username=current_user)
 
-#     if request.method=="POST":
-#         form =BlogPostForm(request.POST,request.FILES)
-#         if form.is_valid():
-#             blogpost = form.save(commit = False)
-#             blogpost.username = current_user
-#             blogpost.neighbourhood = profile.neighbourhood
-#             blogpost.avatar = profile.avatar
-#             blogpost.save()
+    if request.method=="POST":
+        form =BlogPostForm(request.POST,request.FILES)
+        if form.is_valid():
+            blogpost = form.save(commit = False)
+            blogpost.username = current_user
+            blogpost.neighbourhood = profile.neighbourhood
+            blogpost.avatar = profile.avatar
+            blogpost.save()
 
-#         return HttpResponseRedirect('/blog')
+        return HttpResponseRedirect('/blog')
 
-#     else:
-#         form = BlogPostForm()
+    else:
+        form = BlogPostForm()
 
-#     return render(request,'blogpost_form.html',{"form":form})
+    return render(request,'blogpost_form.html',{"form":form})
 
 # @login_required(login_url='/accounts/login/')
 # def new_business(request):
