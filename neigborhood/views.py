391c462452255f1f60e69514c0991afaa2a58580
canssidle <canssidlewairimu@gmail.com>
2019-07-08 11:22:12 +0300

    create the view for index

--- neigborhood/views.py
+++ neigborhood/views.py
@@ -1,31 +1,31 @@
-# from django.shortcuts import render,redirect
-# from django.http import HttpResponse,Http404,HttpResponseRedirect
-# from django.contrib.auth.decorators import login_required
-# from django.core.exceptions import ObjectDoesNotExist
-# from .models import neighbourhood,healthservices,Business,Health,Authorities,BlogPost,Profile,notifications,Comment
-# from .email import send_priority_email
-# from .forms import notificationsForm,ProfileForm,BlogPostForm,BusinessForm,CommentForm
-# from decouple import config,Csv
-# import datetime as dt
-# from django.http import JsonResponse
-# import json
-# from django.db.models import Q
-# from django.contrib.auth.models import User
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import neighbourhood,healthservices,Business,Health,Authorities,BlogPost,Profile,notifications,Comment
+from .email import send_priority_email
+from .forms import notificationsForm,ProfileForm,BlogPostForm,BusinessForm,CommentForm
+from decouple import config,Csv
+import datetime as dt
+from django.http import JsonResponse
+import json
+from django.db.models import Q
+from django.contrib.auth.models import User
 
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# @login_required(login_url='/accounts/login/')
-# def index(request):
-#     try:
-#         if not request.user.is_authenticated:
-#             return redirect('/accounts/login/')
-#         current_user=request.user
-#         profile =Profile.objects.get(username=current_user)
-#     except ObjectDoesNotExist:
-#         return redirect('create-profile')
+@login_required(login_url='/accounts/login/')
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user=request.user
+        profile =Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
 
-#     return render(request,'index.html')
+    return render(request,'index.html')
 
 # @login_required(login_url='/accounts/login/')
 # def notification(request):
