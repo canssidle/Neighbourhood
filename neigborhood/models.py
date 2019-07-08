a6119eaea5452430367f6198d069a2f49bee3ee4
canssidle <canssidlewairimu@gmail.com>
2019-07-08 11:14:18 +0300

    create the business class

--- neigborhood/models.py
+++ neigborhood/models.py
@@ -50,18 +50,18 @@ class healthservices(models.Model):
         cls.objects.filter(healthservices=healthservices).delete()
 
 
-# class Business(models.Model):
-#     logo = models.ImageField(upload_to='businesslogo/')
-#     description = HTMLField()
-#     neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
-#     owner = models.ForeignKey(User,on_delete=models.CASCADE)
-#     name =models.CharField(max_length=100)
-#     email = models.EmailField()
-#     address =models.CharField(max_length=100)
-#     contact = models.IntegerField()
+class Business(models.Model):
+    logo = models.ImageField(upload_to='businesslogo/')
+    description = HTMLField()
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+    address =models.CharField(max_length=100)
+    contact = models.IntegerField()
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 # class Health(models.Model):
 #     logo = models.ImageField(upload_to='healthlogo/')
