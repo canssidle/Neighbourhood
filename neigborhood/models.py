d71ad7d989aec83fb6f750e6bfb952276a9c40a3
canssidle <canssidlewairimu@gmail.com>
2019-07-08 11:13:59 +0300

    create the helthservices class

--- neigborhood/models.py
+++ neigborhood/models.py
@@ -36,18 +36,18 @@ class notifications(models.Model):
         return self.title
 
 
-# class healthservices(models.Model):
-#     healthservices = models.CharField(max_length=100)
+class healthservices(models.Model):
+    healthservices = models.CharField(max_length=100)
 
-#     def __str__(self):
-#         return self.healthservices
+    def __str__(self):
+        return self.healthservices
 
-#     def save_healthservices(self):
-#         self.save()
+    def save_healthservices(self):
+        self.save()
 
-#     @classmethod
-#     def delete_healthservices(cls,healthservices):
-#         cls.objects.filter(healthservices=healthservices).delete()
+    @classmethod
+    def delete_healthservices(cls,healthservices):
+        cls.objects.filter(healthservices=healthservices).delete()
 
 
 # class Business(models.Model):
