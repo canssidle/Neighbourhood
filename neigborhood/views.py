668c3552a48ad87de3b75d2b01a92174d5a48799
canssidle <canssidlewairimu@gmail.com>
2019-07-08 11:23:53 +0300

    create the view for authorities

--- neigborhood/views.py
+++ neigborhood/views.py
@@ -51,13 +51,13 @@ def health(request):
 
     return render(request,'health.html',{"healthservices":healthservices})
 
-# @login_required(login_url='/accounts/login/')
-# def authorities(request):
-#     current_user=request.user
-#     profile=Profile.objects.get(username=current_user)
-#     authorities = Authorities.objects.filter(neighbourhood=profile.neighbourhood)
+@login_required(login_url='/accounts/login/')
+def authorities(request):
+    current_user=request.user
+    profile=Profile.objects.get(username=current_user)
+    authorities = Authorities.objects.filter(neighbourhood=profile.neighbourhood)
 
-#     return render(request,'authorities.html',{"authorities":authorities})
+    return render(request,'authorities.html',{"authorities":authorities})
 
 # @login_required(login_url='/accounts/login/')
 # def businesses(request):
