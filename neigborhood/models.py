19fd5bf19fd7ad2541cb6ea2adc526141a4098b4
canssidle <canssidlewairimu@gmail.com>
2019-07-08 11:14:58 +0300

    create the authorities class

--- neigborhood/models.py
+++ neigborhood/models.py
@@ -75,15 +75,15 @@ class Health(models.Model):
     def __str__(self):
         return self.name
 
-# class Authorities(models.Model):
-#     neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
-#     name =models.CharField(max_length=100)
-#     email = models.EmailField()
-#     contact = models.IntegerField()
-#     address =models.CharField(max_length=100)
+class Authorities(models.Model):
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.IntegerField()
+    address =models.CharField(max_length=100)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 
 # class Profile(models.Model):
