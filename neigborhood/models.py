b6d7322556c453b484b5fa00cca53c3d1cdf8276
canssidle <canssidlewairimu@gmail.com>
2019-07-08 11:13:14 +0300

    create the neighbourhood class

--- neigborhood/models.py
+++ neigborhood/models.py
@@ -1,27 +1,27 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from tinymce.models import HTMLField
-# from django.db.models import Q
-# import datetime as dt
+from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.db.models import Q
+import datetime as dt
 
-# Priority=(
-#     ('Informational','Informational'),
-#     ('High Priority','High Priority'),
-# )
+Priority=(
+    ('Informational','Informational'),
+    ('High Priority','High Priority'),
+)
 
 
-# class neighbourhood(models.Model):
-#     neighbourhood= models.CharField(max_length=100)
+class neighbourhood(models.Model):
+    neighbourhood= models.CharField(max_length=100)
 
-#     def __str__(self):
-#         return self.neighbourhood
+    def __str__(self):
+        return self.neighbourhood
 
-#     def save_neighbourhood(self):
-#         self.save()
+    def save_neighbourhood(self):
+        self.save()
 
-#     @classmethod
-#     def delete_neighbourhood(cls,neighbourhood):
-#         cls.objects.filter(neighbourhood=neighbourhood).delete()
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
 
 
 # class notifications(models.Model):
